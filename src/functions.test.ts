7b96dc957859d29ae68b323c8a3a595d84ef6a25
Eli Ennab <elinaennab@gmail.com>
2023-03-15 09:23:23 +0100

    structure for testing of functions

--- src/functions.test.ts
+++ src/functions.test.ts
@@ -1,1 +1,42 @@
 import { describe, it, expect } from 'vitest'
+
+/**
+ * Add todo
+ */
+describe('add todo', () => {
+
+	it.todo('should add a todo', () => {
+	})
+
+    it.todo('should not add a todo with empty title', () => {
+	})
+
+    it.todo('should not add a todo with title shorter than 3 characters', () => {
+	})
+})
+
+/**
+ * Toggle todo
+ */
+describe('toggle todo', () => {
+
+	it.todo('should toggle a todo', () => {
+	})
+
+    it.todo('should not toggle a todo that does not exist', () => {
+	})
+
+})
+
+/**
+ * Delete todo
+ */
+describe('delete todo', () => {
+
+	it.todo('should delete a todo', () => {
+	})
+
+    it.todo('should not delete a todo that does not exist', () => {
+	})
+
+})
