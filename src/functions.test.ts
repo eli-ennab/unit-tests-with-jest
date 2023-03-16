8de4c9b92ef9d65ecbad352a0e746c7debe7d269
Eli Ennab <elinaennab@gmail.com>
2023-03-16 15:02:37 +0100

    clarifying comments + test for add todo are almost finished

--- src/functions.test.ts
+++ src/functions.test.ts
@@ -1,31 +1,61 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, afterEach } from 'vitest'
 import { addTodo, toggleTodo, deleteTodo } from './functions'
 import { Todo } from './types/Todo'
+
+let todos: Todo[] = []
+
+const newTodoTitle = 'Learn Unit Testing'
+const emptyTodoTitle = ''
+const shortTodoTitle = 'Le'
 
 /**
  * Add todo
  */
 describe('add todo', () => {
 
-    const newTodo = 'Learn Unit Testing'
-    const emptyTodo = ''
-    const shortTodo = 'Le'
+    afterEach(() => {
+		todos = []
+	})
 
 	it('should add a todo', () => {
-        let todos: Todo[] = []
-        addTodo(newTodo, todos)
-        expect(todos).toContainEqual({ title: newTodo, id: 1, completed: false })
+        const result = addTodo(newTodoTitle, todos)
+
+        // can add a todo
+        expect(todos).toContainEqual({ title: newTodoTitle, id: 1, completed: false })
+
+        // has successfully added a todo
+        expect(result.success).toBe(true)
+
+        // the list is not still empty
+        expect(todos.length).toBe(1)
+
+        // the todo added is the todo that was sent in
+
 	})
 
     it('should not add a todo with empty title', () => {
-        const todos: Todo[] = []
-        addTodo(emptyTodo, todos)
+        const result = addTodo(emptyTodoTitle, todos)
+
+        // result could not be added
+        expect(result.success).toBe(false)
+
+        // result could not be added because title was empty
+        expect(result.error).toBe('Title cannot be empty')
+
+        // the list is still empty
         expect(todos).toEqual([])
 	})
 
     it('should not add a todo with title shorter than 3 characters', () => {
-        const todos: Todo[] = []
-        addTodo(shortTodo, todos)
+        const result = addTodo(shortTodoTitle, todos)
+
+        // todo could not be added
+        expect(result.success).toBe(false)
+
+        // result could not be added because title was less than three chars
+        expect(result.error).toBe('Title must be at least 3 characters long')
+
+        // the list is still empty
         expect(todos).toEqual([])
 	})
 
